c5dc3c392e4f3ec89ed15194fce33fa1536f3ce5
k-pradeep <pradeep.k0810@gmail.com>
2023-04-13 23:09:16 -0400

    rebased task B changes

--- revenueforecast/src/TaskB.py
+++ revenueforecast/src/TaskB.py
@@ -1,20 +1,31 @@
 import pathlib
 from datetime import datetime, timedelta
-from helper import write_to_csv
+from helper import write_to_csv, is_date_in_2023
+from dateutil.relativedelta import relativedelta
 
 # Initialize cash in bank as zero for each day of 2023
 cummulative_sum_cash_in_bank = [0] * 365
 current_working_directory = pathlib.Path(__file__).parent.parent.resolve()
 
 
-def calculate_next_billing_date(billing_period_end_date, next_billing_date):
+def calculate_next_billing_date(
+    billing_period_start_date, billing_period_end_date, next_billing_date
+):
     """
-    Function to calculate the next billing date based on the billing period end date and next billing date
+    Function to calculate the next billing date based on the billing period
+    end date and next billing date
     """
-    if next_billing_date is None:
-        return billing_period_end_date
-    else:
-        return next_billing_date
+    if next_billing_date < datetime.strptime(
+        "2024-01-01 00:00:00.000000 UTC", "%Y-%m-%d %H:%M:%S.%f %Z"
+    ):
+        billing_period_start_date = next_billing_date
+
+        # period end is period start + 1 month
+        billing_period_end_date = billing_period_start_date + relativedelta(months=1)
+        # print(f'period_end is {billing_period_end_date}')
+        next_billing_date = billing_period_end_date + timedelta(1)
+
+    return (billing_period_start_date, billing_period_end_date, next_billing_date)
 
 
 def cash_per_day(cash_in_bank, sheet_name):
@@ -35,12 +46,76 @@ def cash_per_day(cash_in_bank, sheet_name):
     write_to_csv(output_csv_file, "w", data, header)
 
 
-def forecast_cash_in_bank():
-    csv_file = pathlib.Path(
-        current_working_directory, "data", "input", "sheet1.csv"
+def get_grace_period(schedule_due_date, schedule_due_date_type):
+    """Generates time period between schedule date date and due date
+
+    Args:
+        schedule_due_date (str): number of days before invoice is due
+        schedule_due_date_type (str): provides due date type either before or after
+
+    Returns:
+        grace_period (datetime): number of days to be added
+    """
+    grace_period = None
+
+    if schedule_due_date_type == "DAYSAFTERBILLDATE":
+        # this holds time interval between schedule date and due date.
+        grace_period = timedelta(int(schedule_due_date))
+
+    return grace_period
+
+
+def calculate_next_scheduled_date(
+    schedule_next_scheduled_date, schedule_period, schedule_unit
+):
+    if schedule_unit == "MONTHLY":
+        schedule_period = relativedelta(months=int(schedule_period))
+
+    schedule_next_scheduled_date = (
+        schedule_next_scheduled_date + schedule_period
     )  # noqa: E501
+
+    # print(f"in calculate next date - {schedule_next_scheduled_date}")
+    return schedule_next_scheduled_date
+
+
+def get_cummulative_cash_per_day_per_dataset(cash_per_day):
+    cummulative_sum_cash_in_bank_per_dataset = [0] * 365
+
+    for days_from_new_year in range(len(cummulative_sum_cash_in_bank_per_dataset)):
+        # revenue gained today
+        cummulative_sum_cash_in_bank_per_dataset[days_from_new_year] = cash_per_day[
+            days_from_new_year
+        ]
+
+        # add yesterday's revenue with today's revenue
+        if days_from_new_year != 0:
+            cummulative_sum_cash_in_bank_per_dataset[days_from_new_year] = (
+                cummulative_sum_cash_in_bank_per_dataset[days_from_new_year]
+                + cummulative_sum_cash_in_bank_per_dataset[days_from_new_year - 1]
+            )
+        else:
+            cummulative_sum_cash_in_bank_per_dataset[
+                days_from_new_year
+            ] = cummulative_sum_cash_in_bank_per_dataset[days_from_new_year]
+
+        # round off by 2 decimal values
+        cummulative_sum_cash_in_bank_per_dataset[days_from_new_year] = round(
+            cummulative_sum_cash_in_bank_per_dataset[days_from_new_year], 2
+        )
+        # round off by 2 decimal values
+        cummulative_sum_cash_in_bank_per_dataset[days_from_new_year] = round(
+            cummulative_sum_cash_in_bank_per_dataset[days_from_new_year], 2
+        )
+    return cummulative_sum_cash_in_bank_per_dataset
+
+
+if __name__ == "__main__":
+    csv_file = pathlib.Path(current_working_directory, "data", "input", "sheet1.csv")
+
     # Process data in Sheet A
-    cash_in_bank = [0] * 365
+    cash_in_bank_dataset_a = [0] * 365
+
     with open(csv_file, "r") as file:
         next(file)  # Skip the header row
         for line in file:
@@ -52,6 +127,9 @@ def forecast_cash_in_bank():
                 next_billing_date,
             ) = line.strip().split(",")
             price = round(float(price), 2)
+            billing_period_start_date = datetime.strptime(
+                billing_period_start_date, "%Y-%m-%d %H:%M:%S.%f %Z"
+            )
             billing_period_end_date = datetime.strptime(
                 billing_period_end_date, "%Y-%m-%d %H:%M:%S.%f %Z"
             )
@@ -60,27 +138,53 @@ def forecast_cash_in_bank():
                 if next_billing_date
                 else None
             )
-            next_billing_date = calculate_next_billing_date(
-                billing_period_end_date, next_billing_date
-            )
-            days_to_next_billing_date = (next_billing_date - datetime(2023, 1, 1)).days
-            for i in range(days_to_next_billing_date):
-                cash_in_bank[i] += 0
-            # cash_in_bank[days_to_next_billing_date] += price
-            cash_in_bank[days_to_next_billing_date] = round(
-                cash_in_bank[days_to_next_billing_date] + price, 2
-            )
-            cummulative_sum_cash_in_bank[days_to_next_billing_date] = round(
-                cummulative_sum_cash_in_bank[days_to_next_billing_date] + price, 2
-            )
+            # these variables is used to forecast renewals
+            period_start = billing_period_start_date
+            period_end = billing_period_end_date
 
-        print(cash_in_bank)
+            # forecasts renewals based on next billing date, price remains same
+            while is_date_in_2023(next_billing_date):
+                days_to_next_billing_date = (
+                    next_billing_date - datetime(2023, 1, 1)
+                ).days
 
+                # adding price cash reserve
+                # for i in range(days_to_next_billing_date):
+                #     cash_in_bank[i] += 0
+                # cash_in_bank[days_to_next_billing_date] += price
+                cash_in_bank_dataset_a[days_to_next_billing_date] = round(
+                    cash_in_bank_dataset_a[days_to_next_billing_date] + price, 2
+                )
+                # need rework
+                # cummulative_sum_cash_in_bank[days_to_next_billing_date] = round(
+                #     cummulative_sum_cash_in_bank[days_to_next_billing_date] + price, 2
+                # )
+                # check for next billing date
+                (
+                    period_start,
+                    period_end,
+                    next_billing_date,
+                ) = calculate_next_billing_date(
+                    period_start, period_end, next_billing_date
+                )
+                print(next_billing_date)
+
+        print(cash_in_bank_dataset_a)
+        print("-----" * 20)
         # cash in bank per day in sheet 1
-        cash_per_day(cash_in_bank, "sheet1.csv")
+        cash_per_day(cash_in_bank_dataset_a, "dataset_a_cash_revenue_per_day.csv")
+
+        # cummulative revenue per dataset
+        cummulative_cash_per_day_per_dataset = get_cummulative_cash_per_day_per_dataset(
+            cash_in_bank_dataset_a
+        )
+        cash_per_day(
+            cummulative_cash_per_day_per_dataset,
+            "dataset_a_cash_revenue_cummulative.csv",
+        )
 
     # Process data in Sheet B
-    cash_in_bank = [0] * 365
+    cash_in_bank_dataset_b = [0] * 365
     csv_file = pathlib.Path(current_working_directory, "data", "input", "sheet2.csv")
     with open(csv_file, "r") as file:
         next(file)  # Skip the header row
@@ -95,31 +199,58 @@ def forecast_cash_in_bank():
                 schedule_due_date_type,
             ) = line.strip().split(",")
             sub_total = float(sub_total)
-            # updating due date
             schedule_next_scheduled_date = datetime.strptime(
                 schedule_next_scheduled_date, "%Y-%m-%d %H:%M:%S"
-            ) + timedelta(int(schedule_due_date))
-            days_to_next_scheduled_date = (
-                schedule_next_scheduled_date - datetime(2023, 1, 1)
-            ).days
-            for i in range(days_to_next_scheduled_date):
-                cash_in_bank[i] += 0
-            # cash_in_bank[days_to_next_scheduled_date] += sub_total
+            )
 
-            cash_in_bank[days_to_next_scheduled_date] = round(
-                cash_in_bank[days_to_next_scheduled_date] + price, 2
+            # calculate grace_period
+            grace_period = get_grace_period(
+                schedule_due_date.strip(), schedule_due_date_type.strip()
             )
-            cummulative_sum_cash_in_bank[days_to_next_scheduled_date] = round(
-                cummulative_sum_cash_in_bank[days_to_next_scheduled_date] + price, 2
-            )
+            # print(
+            #     f"grace_period is {grace_period}, schedule due date is {schedule_next_scheduled_date}"
+            # )
+            if grace_period is None:
+                print(
+                    f"""grace period is none - schedule due date is {schedule_next_scheduled_date}
+                    , schedule_due_date is {schedule_due_date},
+                      schedule_due_date_type is {schedule_due_date_type}"""
+                )
+
+            while is_date_in_2023(schedule_next_scheduled_date + grace_period):
+                # calculate bill due date
+                bill_due_date = schedule_next_scheduled_date + grace_period
+                # print(f"bill_due_date is {bill_due_date}")
+                days_to_bill_due_date = (
+                    bill_due_date - datetime(2023, 1, 1)
+                ).days  # noqa: E501
+
+                cash_in_bank_dataset_b[days_to_bill_due_date] = round(
+                    cash_in_bank_dataset_b[days_to_bill_due_date] + sub_total, 2
+                )
+
+                # Get next scheduled date
+                schedule_next_scheduled_date = calculate_next_scheduled_date(
+                    schedule_next_scheduled_date, schedule_period, schedule_unit
+                )
+
         print("After processing sheet B")
-        print(cash_in_bank)
+        print(cash_in_bank_dataset_b)
+        print("-----" * 20)
+        # cash in bank per day in sheet 1
+        cash_per_day(cash_in_bank_dataset_b, "dataset_b_cash_revenue_per_day.csv")
 
-        # cash in bank per day in sheet 2
-        cash_per_day(cash_in_bank, "sheet2.csv")
+        # cummulative revenue per dataset
+        cummulative_cash_per_day_per_dataset = get_cummulative_cash_per_day_per_dataset(
+            cash_in_bank_dataset_b
+        )
+        cash_per_day(
+            cummulative_cash_per_day_per_dataset,
+            "dataset_b_cash_revenue_cummulative.csv",
+        )
 
     # Process data in Sheet C
-    cash_in_bank = [0] * 365
+    cash_in_bank_dataset_c = [0] * 365
     csv_file = pathlib.Path(current_working_directory, "data", "input", "sheet3.csv")
     with open(csv_file, "r") as file:
         next(file)  # Skip the header row
@@ -136,30 +267,68 @@ def forecast_cash_in_bank():
             sub_total = float(sub_total)
             # convert GBP to USD ,  as per google, 1 GBP = 1.23 USD
             sub_total = round((sub_total * 1.23), 2)
+            # str to datetime object conversion
             schedule_next_scheduled_date = datetime.strptime(
                 schedule_next_scheduled_date, "%Y-%m-%d %H:%M:%S"
-            ) + timedelta(int(schedule_due_date))
-            days_to_next_scheduled_date = (
-                schedule_next_scheduled_date - datetime(2023, 1, 1)
-            ).days
-            for i in range(days_to_next_scheduled_date):
-                cash_in_bank[i] += 0
-            cash_in_bank[days_to_next_scheduled_date] = round(
-                cash_in_bank[days_to_next_scheduled_date] + price, 2
             )
-            cummulative_sum_cash_in_bank[days_to_next_scheduled_date] = round(
-                cummulative_sum_cash_in_bank[days_to_next_scheduled_date] + price, 2
+            # calculate grace_period
+            grace_period = get_grace_period(
+                schedule_due_date.strip(), schedule_due_date_type.strip()
             )
+
+            # print(
+            #     f"grace_period is {grace_period}, schedule due date is {schedule_next_scheduled_date}"
+            # )
+
+            if grace_period is None:
+                print(
+                    f"""grace period is none - schedule due date is {schedule_next_scheduled_date}, 
+                        schedule_due_date is {schedule_due_date},
+                         schedule_due_date_type is {schedule_due_date_type}"""
+                )
+
+            while is_date_in_2023(schedule_next_scheduled_date + grace_period):
+                # calculate bill due date
+                bill_due_date = schedule_next_scheduled_date + grace_period
+                # print(f"bill_due_date is {bill_due_date}")
+
+                days_to_bill_due_date = (
+                    bill_due_date - datetime(2023, 1, 1)
+                ).days  # noqa: E501
+
+                cash_in_bank_dataset_c[days_to_bill_due_date] = round(
+                    cash_in_bank_dataset_c[days_to_bill_due_date] + sub_total, 2
+                )
+                # Get next scheduled date
+                schedule_next_scheduled_date = calculate_next_scheduled_date(
+                    schedule_next_scheduled_date, schedule_period, schedule_unit
+                )
 
         print("After processing sheet3")
-        print(cash_in_bank)
+        print(cash_in_bank_dataset_c)
 
         # cash in bank per day in sheet 3
-        cash_per_day(cash_in_bank, "sheet3.csv")
+        cash_per_day(cash_in_bank_dataset_c, "dataset_c_cash_revenue_per_day.csv")
+
+        # cummulative revenue per dataset
+        cummulative_cash_per_day_per_dataset = get_cummulative_cash_per_day_per_dataset(
+            cash_in_bank_dataset_c
+        )
+        cash_per_day(
+            cummulative_cash_per_day_per_dataset,
+            "dataset_c_cash_revenue_cummulative.csv",
+        )
 
         # cummulative sum
         for days_from_new_year in range(len(cummulative_sum_cash_in_bank)):
-            # generate cummulative sum by adding current day revenue from sheets with yesterday's revenue subtotal
+            # Today's earnings across datasets
+            cummulative_sum_cash_in_bank[days_from_new_year] = (
+                cash_in_bank_dataset_a[days_from_new_year]
+                + cash_in_bank_dataset_b[days_from_new_year]
+                + cash_in_bank_dataset_c[days_from_new_year]
+            )
+
+            # generate cummulative sum by adding current day revenue with yesterday's revenue subtotal
             if days_from_new_year != 0:
                 cummulative_sum_cash_in_bank[days_from_new_year] = (
                     cummulative_sum_cash_in_bank[days_from_new_year]
@@ -169,6 +338,11 @@ def forecast_cash_in_bank():
                 cummulative_sum_cash_in_bank[
                     days_from_new_year
                 ] = cummulative_sum_cash_in_bank[days_from_new_year]
+
+            # round off by 2 decimal values
+            cummulative_sum_cash_in_bank[days_from_new_year] = round(
+                cummulative_sum_cash_in_bank[days_from_new_year], 2
+            )
 
         # write to csv
         print(cummulative_sum_cash_in_bank)
